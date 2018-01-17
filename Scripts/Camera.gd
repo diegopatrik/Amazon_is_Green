35dd524da447f0df323e10a02faef5e6e1868a27
Diego Patrik <dpatrikone@gmail.com>
2018-01-17 12:21:45 -0200

    movimento camera
    parece ok agr, falta testar no celular

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -8,4 +8,8 @@ func _ready():
 	set_process_input(true)
 
 func _on_SwipeDetector_swiped( gesture ):
-	set_pos(gesture.last_point())
+	set_global_pos(gesture.last_point())
+	print(gesture.get_distance())
+
+func _on_SwipeDetector_swipe_updated( partial_gesture ):
+	set_global_pos(partial_gesture.last_point())
